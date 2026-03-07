fd37ea520ec5a16935d6bc125f41128b73030b36
Dennis Luebke <github@dennis2society.de>
2026-03-07 12:32:12 +0100

    - added application icon

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include "streamstatemanager.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("QtRtspViewer"));
     app.setOrganizationName(QStringLiteral("QtRtspViewer"));
+    app.setWindowIcon(QIcon(QStringLiteral(":/qt_rtsp_viewer_icon.png")));
 
     // Ensure the singleton is alive and settings are loaded
     StreamStateManager::instance();
